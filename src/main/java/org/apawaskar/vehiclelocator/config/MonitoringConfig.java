997714344254fce28dfa57cb36e51b5a44ece98c
Akshay Pawaskar <akshay.abp@gmail.com>
2020-01-05 17:20:27 -0500

    Updated spring boot and spring cloud version. Added promethesus and logstash features.

---
+++ src/main/java/org/apawaskar/vehiclelocator/config/MonitoringConfig.java
@@ -0,0 +1,20 @@
+package org.apawaskar.vehiclelocator.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+
+@Configuration
+public class MonitoringConfig {
+	
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+	  return registry -> registry.config().commonTags("application", applicationName);
+	}
+}
